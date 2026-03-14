0748f571e1d1a3e0f199a92d19cb105ac09ab00f
HariprasadNuttu <hariprasadnuttu@gmail.com>
2026-03-14 07:00:37 +0530

    changesS

--- start.sh
+++ start.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-# set -e
-# cd server
-# npm install
-# npx prisma generate
-# npm run deploy:build
-# npx prisma db push --skip-generate || true
-# exec node dist/index.js
-#!/bin/bash
 set -e
 cd server
-npx prisma db seed
+npm install
+npx prisma generate
+npm run deploy:build
+npx prisma db push --skip-generate || true
 exec node dist/index.js
